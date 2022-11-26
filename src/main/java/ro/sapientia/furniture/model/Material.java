62555235b32790c060d85ef61ea28a015e4425f6
Endre Toducz <endre.toducz@electrolux.com>
2022-11-26 16:49:59 +0200

    create Material entity

---
+++ src/main/java/ro/sapientia/furniture/model/Material.java
@@ -0,0 +1,22 @@
+package ro.sapientia.furniture.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity(name = "material")
+public class Material implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="pk_material")
+    @SequenceGenerator(name="pk_material",sequenceName="pk_material")
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "type")
+    private int type;
+
+    @Column(name = "price")
+    private double price;
+}
