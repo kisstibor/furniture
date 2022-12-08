87cd8b6ee94eacae185a7ff86d4b47b14894b0ae
mesteri15micro <tanzoli2@yahoo.com>
2022-12-09 00:51:26 +0200

    initial commit

---
+++ src/main/java/ro/sapientia/furniture/service/SettingsService.java
@@ -0,0 +1,9 @@
+package ro.sapientia.furniture.service;
+
+public class SettingsService {
+	public static void main(String[] args) {
+		System.out.println("get actual settings");
+		System.out.println("modify settings");
+		}
+
+}
