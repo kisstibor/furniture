7e5caba231740aeb24f07cb864ee52519204259c
mesteri15micro <tanzoli2@yahoo.com>
2022-12-12 23:32:27 +0200

    settings-service api Berecki Zoltan

--- src/main/java/ro/sapientia/furniture/service/SettingsService.java
+++ src/main/java/ro/sapientia/furniture/service/SettingsService.java
@@ -1,7 +1,13 @@
 package ro.sapientia.furniture.service;
 
-public class SettingsService {
-	public static void main(String[] args) {
+import java.io.IOException;
+
+public class SettingsService extends OptionMenuForSettingsService{
+	public static void main(String[] args) throws IOException {
+		OptionMenuForSettingsService optionMenuForSettingsService = new OptionMenuForSettingsService();
+		
+		optionMenuForSettingsService.getLogin();
+		
 		System.out.println("get actual settings");
 		System.out.println("modify settings");
 		}
