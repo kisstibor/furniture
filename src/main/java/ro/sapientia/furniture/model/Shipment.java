d073c5a84da67a1f58bb5925a0f5fff270defae4
dkadrienn <deak.adrienn10@gmail.com>
2022-12-30 12:32:54 +0200

    Using lombok instead generating getters and setters

--- src/main/java/ro/sapientia/furniture/model/Shipment.java
+++ src/main/java/ro/sapientia/furniture/model/Shipment.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
+import lombok.Data;
+
+@Data
 @Entity(name = "shipment")
 public class Shipment {
 	
@@ -32,62 +35,14 @@ public class Shipment {
 
 	@Column(name = "postCode")
 	private int postCode;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	
-	public String getNr() {
-		return nr;
-	}
-
-	public void setNr(String nr) {
-		this.nr = nr;
-	}
-	
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-	
-	public int getPostCode() {
-		return postCode;
-	}
-
-	public void setPostCode(int postCode) {
-		this.postCode = postCode;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 	
 	@Override 
 	public String toString() {
 		return "Shipment [id=" + id + ", order id=" + orderId + ", shipping adress=" + city + street + nr + "[postcode:" + postCode + " ]" + "]";
+	}
+
+	public static long getSerialversionUid() {
+		return serialVersionUID;
 	}
 
 }
