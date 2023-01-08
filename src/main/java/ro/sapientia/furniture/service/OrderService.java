f54991a4daa70b595d7a87af3f03e8ae095a82c1
Endre Csorvasi <endrecsorvasi@gmail.com>
2023-01-08 18:54:15 +0200

    Added exception checks in OrderService

--- src/main/java/ro/sapientia/furniture/service/OrderService.java
+++ src/main/java/ro/sapientia/furniture/service/OrderService.java
@@ -23,19 +23,39 @@ public class OrderService {
 	}
 	
 	public Order findeOrderById(final Long id) {
-		return this.orderRepository.findById(id).orElseThrow();
+		return this.orderRepository.findById(id).orElseThrow(
+				()->new RuntimeException("User with the given id [ " + id + " ] is not found!"));
 	}
 	
 	public Order create(final Order order) {
-		return this.orderRepository.saveAndFlush(order);
+		try {
+			return this.orderRepository.saveAndFlush(order);
+		}
+		catch(Exception ex) {
+			System.out.println("Erro occurred when saving new entity.  With the next error: " + ex.getLocalizedMessage());
+			return null;
+		}
+		
+		
 	}
 	
 	public Order update(final Order order) {
+		try {
 		return this.orderRepository.saveAndFlush(order);
+		}
+		catch(Exception ex) {
+			System.out.println("Erro occurred when updating entity with id [ "+ order.getId()+" ].  With the next error: " + ex.getLocalizedMessage());
+			return null;
+		}
 	}
 	
 	public void delete(final Long id) {
+		try {
 		this.orderRepository.deleteById(id);
+		}
+		catch(Exception ex) {
+			System.out.println("Erro occurred when deleting entity with id [ "+ id +" ].  With the next error: " + ex.getLocalizedMessage());
+		}
 	}
 	
 }
