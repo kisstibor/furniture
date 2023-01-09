160cf41fe7341e0d8bf2f8e1926e7862c37ec841
Endre Csorvasi <endrecsorvasi@gmail.com>
2023-01-09 23:25:49 +0200

    Created Order repository tests

--- src/main/java/ro/sapientia/furniture/model/OrderEntity.java
+++ src/main/java/ro/sapientia/furniture/model/OrderEntity.java
@@ -21,8 +21,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name="order")
-
+@Entity(name="order_entity")
 public class OrderEntity implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
--- src/test/java/ro/sapientia/furniture/repository/OrderRepositoryTest.java
+++ src/test/java/ro/sapientia/furniture/repository/OrderRepositoryTest.java
@@ -1,18 +1,18 @@
 package ro.sapientia.furniture.repository;
 
+
+
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import ro.sapientia.furniture.model.OrderEntity;
@@ -26,10 +26,12 @@ public class OrderRepositoryTest {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	private OrderEntity order;
+	
 	@BeforeEach
 	public void init() {
-		final OrderEntity order = new OrderEntity(1l, LocalDate.now(),LocalDate.now().plusDays(12),781.90,OrderStatus.ORDERED);
-		orderRepository.saveAndFlush(order);
+		order = new OrderEntity(1l,LocalDate.now(),LocalDate.now().plusDays(12),781.90,OrderStatus.ORDERED);
+		order = orderRepository.saveAndFlush(order);
 	}
 	
 	
@@ -37,6 +39,38 @@ public class OrderRepositoryTest {
 	public void findAll() {	
 		List<OrderEntity> result = orderRepository.findAll();
 		assertEquals(1,result.size());
+	}
+	
+	@Test
+	public void testfindByIdShouldReturnObjectWithTheGivenId() {
+		assertEquals(order.getId(),orderRepository.findById(order.getId()).get().getId());
+	}
+	
+	@Test
+	public void testCreateShouldSaveNewObject() {
+		final OrderEntity order1 = new OrderEntity(10l, LocalDate.now(),LocalDate.now().plusDays(132),1980.90,OrderStatus.ORDERED);
+		orderRepository.saveAndFlush(order1);
+		
+		assertEquals(2,orderRepository.findAll().size());
+	}
+	
+	@Test
+	public void testUpdateShouldUpdateObject() {
+		
+		order.setPrice(431.12);
+		order = orderRepository.saveAndFlush(order);
+		
+		final double newPrice = orderRepository.findById(order.getId()).get().getPrice();
+		
+		assertEquals(431.12,newPrice,2);
+	}
+	
+	@Test
+	public void testDeleteShouldRemoveObject() {		
+		orderRepository.delete(order);
+		
+		
+		assertEquals(0,orderRepository.findAll().size());
 	}
 	
 	@AfterEach
