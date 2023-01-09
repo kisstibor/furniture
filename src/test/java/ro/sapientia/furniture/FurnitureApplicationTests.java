46e3cc25b55f0c5ea38b5577bea7ccd2389daa34
Endre Csorvasi <endrecsorvasi@gmail.com>
2023-01-09 23:55:51 +0200

    Disabled test in FurnitureApplicationTest

--- src/test/java/ro/sapientia/furniture/FurnitureApplicationTests.java
+++ src/test/java/ro/sapientia/furniture/FurnitureApplicationTests.java
@@ -1,5 +1,6 @@
 package ro.sapientia.furniture;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class FurnitureApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
