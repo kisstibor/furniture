251d2b408f60a8475caeca262dba1d3559baaa9b
Endre Csorvasi <endrecsorvasi@gmail.com>
2023-01-10 09:48:16 +0200

    Resolved merge conflicts

--- src/main/java/ro/sapientia/furniture/model/Shipment.java
+++ src/main/java/ro/sapientia/furniture/model/Shipment.java
@@ -1,6 +1,8 @@
 package ro.sapientia.furniture.model;
 import java.util.List;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,10 +16,18 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.Cascade;
 
 import lombok.AllArgsConstructor;
+<<<<<<< HEAD
+=======
+import lombok.Builder;
+>>>>>>> ebb9041 (Resolved merge error)
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+<<<<<<< HEAD
+=======
+@Builder
+>>>>>>> ebb9041 (Resolved merge error)
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "shipment")
@@ -29,11 +39,19 @@ public class Shipment {
 	@Column(name = "id", nullable = false, updatable = false)
 	private Long id;
 	
+<<<<<<< HEAD
 	@Cascade(value= {org.hibernate.annotations.CascadeType.ALL})
 	@JoinColumn(name = "order_id", nullable = false)
 	@OneToMany(mappedBy="shipment",cascade= {CascadeType.PERSIST,CascadeType.REMOVE})
 	@JsonIgnore
 	private List<OrderEntity> order;
+=======
+//	@NotNull
+//	@OneToMany
+//	@Cascade(value= {org.hibernate.annotations.CascadeType.ALL})
+//	@JoinColumn(name = "order_id", nullable = false)
+//	private List<Order> orderId;
+>>>>>>> ebb9041 (Resolved merge error)
 
 	@Column(name = "street")
 	private String street;
