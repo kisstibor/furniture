5f80d4fb6922cefeeca933d87eeedff88439aafc
dkadrienn <deak.adrienn10@gmail.com>
2023-01-11 21:29:20 +0200

    Added BDD tests

--- src/eetest/java/ro/sapientia/furniture/bdt/definition/ShipmentStepDefinition.java
+++ src/eetest/java/ro/sapientia/furniture/bdt/definition/ShipmentStepDefinition.java
@@ -44,33 +44,54 @@ public class ShipmentStepDefinition {
 	@Autowired
 	private TestEntityManager entityManager;
 	
-	@Then("^I should get \"([^\"]*)\" items$")
-	public void IShouldGetTheCorrectNumberOfItems(final int expectedSize) throws Throwable {
-		mvc.perform(get("/billingEntity/all")
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk())
-		.andExpect(content()
-				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-		.andExpect(jsonPath("$", hasSize(expectedSize)));
+	@Given("I have multiple shipments")
+	public void given_multiple_shipments() {
+	    shipments = shipmentService.findAllShipments();
 	}
-	
-	@Then("^I should get the deposit \"([^\"]*)\" for the position \"([^\"]*)\"$")
-    public void IShouldGetTheCorrectDeposit(final BigDecimal expectedResult, final int expectedPosition) throws Throwable {
-        mvc.perform(get("/billingEntity/all")
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(content()
-                .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$[" + expectedPosition + "].totalPrice", is(expectedResult.doubleValue())));
-    }
-	
-	@Then("^I should get the customer name  \"([^\"]*)\" for the position \"([^\"]*)\"$")
-	public void IShouldGetTheCorrectCustomerName(final String expectedResult, final int expectedPosition) throws Throwable {
-		mvc.perform(get("billingEntity/all")
-				.contentType(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-            .andExpect(content()
-                    .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$[" + expectedPosition + "].totalPrice", is(expectedResult.toString())));
+
+	@When("I retrieve all shipments")
+	public void when_retrieve_all_shipments() {
+	    // No action is needed, as the shipments are retrieved in the given step
+	}
+
+	@Then("I should be able to see all the shipments")
+	public void then_verify_all_shipments() {
+	    assertTrue(shipments.size() > 0);
+	}
+
+	@Given("I have a shipment with id {long}")
+	public void given_shipment_by_id(long id) {
+	    shipment = shipmentService.findShipmentById(id);
+	}
+
+	@When("I retrieve the shipment by id")
+	public void when_retrieve_shipment_by_id() {
+	    // No action is needed, as the shipment is retrieved in the given step
+	}
+
+	@Then("I should be able to see the shipment with id {long}")
+	public void then_verify_shipment_by_id(long id) {
+	    assertEquals(shipment.getId(), id);
+	}
+
+	@When("I update the shipment street to {string}")
+	public void when_update_shipment(String street) {
+	    shipment.setStreet(street);
+	    shipment = shipmentService.update(shipment);
+	}
+
+	@Then("the shipment should be updated successfully")
+	public void then_verify_shipment_updated() {
+	    assertEquals(shipment.getStreet(), "Szezam");
+	}
+
+	@When("I delete the shipment with id {long}")
+	public void when_delete_shipment(long id) {
+	    shipmentService.delete(id);
+	}
+
+	@Then("the shipment should be deleted successfully")
+	public void then_verify_shipment_deleted() {
+	    assertFalse(shipmentService.findAllShipments().contains(shipment));
 	}
 }
